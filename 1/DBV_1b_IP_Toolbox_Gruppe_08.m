d2201e7083b6f2c6bcc1713ab7b42ab1e816199d
Stefan Giggenbach <stefan.giggenbach@hm.edu>
2012-04-04 15:01:53 +0200

    Notepad für Aufgabe 1b verwendet

--- 1/DBV_1b_IP_Toolbox_Gruppe_08.m
+++ 1/DBV_1b_IP_Toolbox_Gruppe_08.m
@@ -16,7 +16,7 @@
 %    1.DBV-Ex_Matlab+IPTB.pdf
 %
 
-% ------------------------------------------------------------------------
+% -------------------------------------------------------------------------
 %% Initialisierung:
 coder = 'Cinepak';    %'None', 'Cinepak', 'Indeo3'
 addpath('../allg/');
@@ -31,42 +31,50 @@ figure, imshow(bild);
 %--------------------------------------------------------------------------
 %% Ermitteln Sie folgende Bildinformationen:
 % Zahl der Dimensionen:
-dim = ndims(bild);
+dim = ndims(bild)
 % Bildgr��e in den einzelnen Dimensionen:
-[y,x,d] = size(bild);
+[y,x,k] = size(bild)
 % Datentyp des Bildes:
-???
+desc = whos('bild');
+typ = desc.class
 
 %--------------------------------------------------------------------------
 %% Speichern Sie das Bild als JPEG-Datei in Ergebnisse/ ab
-???
+imwrite(bild,'Ergebnisse/peppers.jpg','jpg');
 
 %--------------------------------------------------------------------------
 %% Trennen sie das Bild in die einzelnen Farbkan�le (rot, gr�n, blau).
 %  Stellen Sie diese dar und speichern Sie diese ebenfalls ab
-bild_r = ???
-bild_g = ???
-bild_b = ???
+bild_r = bild(:,:,1);
+bild_g = bild(:,:,2);
+bild_b = bild(:,:,3);
 
-???
+figure, imshow(bild_r);
+figure, imshow(bild_g);
+figure, imshow(bild_b);
+
+imwrite(bild_r,'Ergebnisse/peppers_r.jpg','jpg');
+imwrite(bild_g,'Ergebnisse/peppers_g.jpg','jpg');
+imwrite(bild_b,'Ergebnisse/peppers_b.jpg','jpg');
 
 %% Was f�llt Ihnen auf bzgl. des Rot- und Blauanteils in den
 %  verschiedenen Bildern?
-%A: ???
-%A: ???
+%A: Der Rotanteil im Bild ist wesentlich höher als der Blauanteil
+%   -> Der rote Farbkanal weist höhere Werte auf (Grauwertbild ist heller)
 
 %--------------------------------------------------------------------------
 %% Greifen Sie aus dem roten Teilbild die 100-te Zeile heraus und
 %  stellen Sie sie als Funktion �ber der x-Koordinate dar;
 %  Markieren Sie diese Zeile geeignet im Rotkanalbild und stellen 
 %  Sie dieses ebenfalls dar:
-zeile_r = ???
-???
-bild_r2 = ???
-???
+zeile_r = bild_r(100,:);
+figure, plot(zeile_r);
+bild_r2 = bild_r;
+bild_r2(100,:) = 0;
+figure, imshow(bild_r2);
 
 %% Speichern Sie die Ergebnisse ab:
-???
+%publish('DBV_1b_IP_Toolbox_Gruppe_08.m');
 
 %%
 'fertig'
