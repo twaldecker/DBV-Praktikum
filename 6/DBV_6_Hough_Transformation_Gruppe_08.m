2fafe5192321ae6777328b44d7cf72d7d36a0f89
Stefan Giggenbach <stefan.giggenbach@hm.edu>
2012-06-21 16:24:19 +0200

    hough hough hurray

--- 6/DBV_6_Hough_Transformation_Gruppe_08.m
+++ 6/DBV_6_Hough_Transformation_Gruppe_08.m
@@ -24,7 +24,7 @@ addpath('../allg/Bilder');
 
 %--------------------------------------------------------------------------
 %% Bild einlesen:
-g = im2single( imread('Geldstücke-klein.pgm' ) );
+g = im2single( imread( 'Geldstücke-klein.pgm' ) );
 fig1 = figure( 1 ); imshow( g );
 
 %--------------------------------------------------------------------------
@@ -36,6 +36,7 @@ fig1 = figure( 1 ); imshow( g );
 % Canny-"Operator" liefert sehr gute Ergebnisse
 gk = edge( g, 'canny', 0.6, 2 );
 fig2 = figure( 2 ); imshow( gk );
+hold on;
 imwrite( gk, 'Ergebnisse/Kantenbild.tif' );
 
 %--------------------------------------------------------------------------
@@ -55,7 +56,8 @@ imwrite( gk, 'Ergebnisse/Kantenmaske.tif' );
 % Folgende Vorkenntnissen sind vorhanden:
 % - die Objekte sind mindestens 25 Pixel von den Bildrändern entfernt
 % - die Kreisradien liegen im Intervall [10,20]
-%???
+
+A = zeros( [size( g ), 11] );
 
 %--------------------------------------------------------------------------
 %% Berechnung Hough-Transformation:
@@ -64,7 +66,25 @@ imwrite( gk, 'Ergebnisse/Kantenmaske.tif' );
 % - Kreismittelpunkte: delta_mx = 3, delta_my = 3
 % - Kreisradien: delta_r = 1
 % Es genügt, nur jeweils jeden zweiten Pixel in x/y-Richtung zu verwenden.
-%???
+
+% Finde alle Pixel mit dem Wert 1 im Binärbild
+ind = find( gk );
+% Für alle Pixel mit Wert 1
+for i = 1:size( ind )
+    [y,x] = ind2sub( size( gk ), ind( i ) );
+    % Für alle möglichen Mittelpunkte
+    for j = 1:3:size( g, 1 )
+        for k = 1:3:293 %size( g, 2 )
+            % Radien berechnen und ggf. speichern
+            r = round( norm( [y,x] - [j,k], 2 ) );
+            if( r >= 10 && r <= 20 )
+                A(j,k,r-9) = A(j,k,r-9) + 1;
+            end
+        end
+    end
+end
+
+
 
 %--------------------------------------------------------------------------
 %% Parameterraum auf erlaubten Bereich einschränken:
@@ -76,14 +96,30 @@ imwrite( gk, 'Ergebnisse/Kantenmaske.tif' );
 %% Ermittlung geeigneter Kreiskandidaten:
 % - suchen Sie nun in der Hough-Transformierten die ersten 9 Maxima,
 % - merken sich die entsprechenden Parameterwerte
-%???
+
+ind = find( A == max( A(:) ) );
+[y,x,r] = ind2sub( size( A ), ind( 1 ) );
+P( 1, : ) = [y,x,r+9];
+A( y, x, r ) = 0;
+for i = 1:8
+    ind = find( A == max( A(:) ) );
+    [y,x,r] = ind2sub( size( A ), ind( 1 ) );
+    P = vertcat( P , [y,x,r+9] );
+    A( y, x, r ) = 0;
+end
 
 %--------------------------------------------------------------------------
 %% Kreisdarstellungen:
 % Zeichnen Sie die Mittelpunkte und Kreise der gefundenen Objekte grün in
 % das Originalbild ein und speichern es ab: 'Ergebnisse/Hough-Kreise.tif';
-%???
 
+phi = 0:0.05:2*pi;
+
+for i = 1:9
+    xk = ( P( i, 3 ) * cos( phi ) ) + P( i, 2 );
+    yk = ( P( i, 3 ) * sin( phi ) ) + P( i, 1 );
+    plot(xk,yk);
+end
 %--------------------------------------------------------------------------
 %% Fragen: 
 % Angenommen, alle Punkte einer Kante werden in der Kantenmaske erfaßt.
